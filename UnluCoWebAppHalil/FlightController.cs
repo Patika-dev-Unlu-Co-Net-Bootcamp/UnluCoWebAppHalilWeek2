4a6cb62ecd0a3140e4ae0e9327f87f3dba5ac9eb
blthll <bolat.halil@std.izu.edu.tr>
2022-01-23 04:47:53 +0300

    Add project files.

---
+++ UnluCoWebAppHalil/FlightController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCoWebAppHalil.Model;
+
+namespace UnluCoWebAppHalil
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private static List<FlightTicket> FlightList = new List<FlightTicket>()
+        {
+            new FlightTicket("Halil","Bolat",1,"TK0008",default,true),
+            new FlightTicket("Enes","Turhan",2,"TK0007",default,false),
+            new FlightTicket("Aynur","Bolat",3,"TK0009",default,true)
+        };
+        [HttpGet]
+        public List<FlightTicket> GetFlightTickets()
+        {
+            return FlightList;
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetFlightTicket(int id)
+        {
+            var ticket = FlightList.Where(x => x.Id == id).FirstOrDefault();
+            if(ticket == null)
+            {
+                return BadRequest(ticket);
+            }
+            return Ok(ticket);
+        }
+        [HttpPost]
+        public ActionResult AddFlightTicket([FromBody]FlightTicket newflight)
+        {
+            FlightList.Add(newflight);
+            return Ok();
+        } 
+        [HttpDelete]
+        public ActionResult DeleteFlightTicket(int id)
+        {
+            var ticket=FlightList.Where(x=>x.Id==id).FirstOrDefault();
+            FlightList.Remove(ticket);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public ActionResult UpdateFlightTicket(int id,[FromBody]FlightTicket flightTicket)
+        {
+            var ticket = FlightList.Any(x => x.Id == id);
+            var ticket2 =FlightList.Where(x => x.Id == id).FirstOrDefault();
+            if (ticket)
+            {
+                ticket2.Id = flightTicket.Id;
+                ticket2.Name = flightTicket.Name;
+                ticket2.Surname = flightTicket.Surname;
+                ticket2.FlightCode = flightTicket.FlightCode;
+                ticket2.FlightDate = flightTicket.FlightDate;
+                return Ok();
+            }
+            return BadRequest();
+        }
+        [HttpPatch] 
+        public ActionResult PatchFlightTicket(int id, [FromBody] FlightTicket flightTicket)
+        {
+            var ticket = FlightList.Any(x => x.Id == id);
+            var ticket2 = FlightList.Where(x => x.Id == id).FirstOrDefault();
+            if (ticket)
+            {
+                ticket2.Id = flightTicket.Id;
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+   
+}
---
+++ UnluCoWebAppHalil/Model/FlightTicket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnluCoWebAppHalil.Model
+{
+    public class FlightTicket
+    {
+        public FlightTicket(string name, string surname, int id, string flightCode, DateTime flightDate,bool isActive)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Id = id;
+            this.FlightCode = flightCode;
+            this.FlightDate = flightDate;
+            this.IsActive = isActive;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public string FlightCode { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime FlightDate { get; set; }
+
+    }
+}
